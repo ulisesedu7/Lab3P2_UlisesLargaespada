6b700f47dd73b28cb771e0e21e26d3c1ff972a41
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-02-03 16:31:26 -0600

    Set up method to add companies and list them

--- src/lab3p2_uliseslargaespada/Lab3P2_UlisesLargaespada.java
+++ src/lab3p2_uliseslargaespada/Lab3P2_UlisesLargaespada.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class Lab3P2_UlisesLargaespada {
 
+  // Set up scanner as a global variable
+  public static Scanner entry = new Scanner(System.in);
+
   public static ArrayList<Vehiculo> vehiculos = new ArrayList();
   public static ArrayList<Cliente> clientes = new ArrayList();
   public static ArrayList<Concesionaria> concesionarias = new ArrayList();
@@ -15,9 +18,6 @@ public class Lab3P2_UlisesLargaespada {
 
   // Main Menu method
   public static void mainMenu() {
-    // Initialize scanner
-    Scanner entry = new Scanner(System.in);
-
     System.out.println("-- Bienvenido al Laboratorio #3 de Program II --");
     System.out.println("En este laboratorio vamos a tener la oportunidad de manejar el registro y control de empresas, vehiculos y clientes para la AMDC");
 
@@ -36,18 +36,22 @@ public class Lab3P2_UlisesLargaespada {
 
       System.out.print("Ingrese la opción: ");
       int option = entry.nextInt();
-      
+
       switch (option) {
         case 1 -> {
+          crudConcesionaria();
         }
 
         case 2 -> {
+          crudClientes();
         }
 
         case 3 -> {
+          crudVehiculos();
         }
 
         case 4 -> {
+          compraVentasPorCliente();
         }
 
         case 5 -> {
@@ -65,5 +69,201 @@ public class Lab3P2_UlisesLargaespada {
     } while (true);
   }
 
-  // Method for the CRUD
+  // Method for the CRUD Concesionaria
+  public static void crudConcesionaria() {
+    System.out.println("Bienvenido al menu para Concensionarias");
+    System.out.println("Aqui usted podra realizar diversas opciones para las concencionarias");
+
+    do {
+      System.out.println("Elija que opcion deseas: ");
+      System.out.println("1 - Crear una Concensionaria");
+      System.out.println("2 - Ver las Concensionarias disponibles");
+      System.out.println("3 - Modificar alguna Concensionaria");
+      System.out.println("4 - Eliminar cualquier Concensionaria");
+      System.out.println("5 - Regresar al Menu Principal \n");
+
+      System.out.print("Ingrese la opción: ");
+      int option = entry.nextInt();
+
+      switch (option) {
+        case 1 -> {
+          System.out.println("Excelente, vmaos a ingresar una concesionaria! \n");
+
+          Concesionaria nuevaC = new Concesionaria();
+
+          System.out.print("Por vavor, indique el nombre de su empresa: ");
+          String nombreC = entry.nextLine();
+          nuevaC.setNombreEmpresa(nombreC);
+
+          System.out.print("Ahora, diganos la direccion de su empresa: ");
+          String direccion = entry.nextLine();
+          nuevaC.setDireccion(direccion);
+
+          System.out.print("Para finalizar, cuanto saldo tendrian su empresa: ");
+          float saldo = entry.nextFloat();
+          nuevaC.setSaldo(saldo);
+
+          // Establecer el Id
+          double id = concesionarias.size() + 1;
+          nuevaC.setId(id);
+
+          concesionarias.add(nuevaC);
+        }
+
+        case 2 -> {
+          listarArrayList(1);
+        }
+
+        case 3 -> {
+        }
+
+        case 4 -> {
+          System.out.println("");
+        }
+
+        case 5 -> {
+          System.out.println("Regresando al Menu Principal \n");
+        }
+
+        default ->
+          System.out.println("Elija una opcion de las anteriores");
+      }
+
+      // Salir del programa 
+      if (option == 5) {
+        break;
+      }
+    } while (true);
+  }
+
+  // Method for the CRUD Clientes
+  public static void crudClientes() {
+    System.out.println("Bienvenido al menu para Clientes");
+    System.out.println("Aqui usted podra realizar diversas opciones para los clientes");
+
+    do {
+      System.out.println("Elija que opcion deseas: ");
+      System.out.println("1 - Crear una Cliente");
+      System.out.println("2 - Ver los Clientes existentes");
+      System.out.println("3 - Modificar algun Cliente");
+      System.out.println("4 - Eliminar a un Cliente");
+      System.out.println("5 - Regresar al Menu Principal \n");
+
+      System.out.print("Ingrese la opción: ");
+      int option = entry.nextInt();
+
+      switch (option) {
+        case 1 -> {
+        }
+
+        case 2 -> {
+        }
+
+        case 3 -> {
+        }
+
+        case 4 -> {
+        }
+
+        case 5 -> {
+          System.out.println("Regresando al Menu Principal \n");
+        }
+
+        default ->
+          System.out.println("Elija una opcion de las anteriores");
+      }
+
+      // Salir del programa 
+      if (option == 5) {
+        break;
+      }
+    } while (true);
+  }
+
+  // Method for the CRUD Vehiculos
+  public static void crudVehiculos() {
+    System.out.println("Bienvenido al menu para Vehiculos");
+    System.out.println("Aqui usted podra realizar diversas opciones para los Vehiculos");
+
+    do {
+      System.out.println("Elija que opcion deseas: ");
+      System.out.println("1 - Agregar un Vehiculo");
+      System.out.println("2 - Ver los Vehiculos disponibles");
+      System.out.println("3 - Modificar algun Vehiculo");
+      System.out.println("4 - Eliminar un Vehiculo");
+      System.out.println("5 - Regresar al Menu Principal \n");
+
+      System.out.print("Ingrese la opción: ");
+      int option = entry.nextInt();
+
+      switch (option) {
+        case 1 -> {
+        }
+
+        case 2 -> {
+        }
+
+        case 3 -> {
+        }
+
+        case 4 -> {
+        }
+
+        case 5 -> {
+          System.out.println("Regresando al Menu Principal \n");
+        }
+
+        default ->
+          System.out.println("Elija una opcion de las anteriores");
+      }
+
+      // Salir del programa 
+      if (option == 5) {
+        break;
+      }
+    } while (true);
+  }
+
+  // Metodo para listar cualquier arrayList
+  public static void listarArrayList(int option) {
+    switch (option) {
+      case 1 -> {
+        if (concesionarias.isEmpty()) {
+          System.out.println("No hay ninguna Concensionaria en lista");
+        } else {
+          System.out.println("Le mostraremos las concesionarias presentes");
+          for (Concesionaria co : concesionarias) {
+            System.out.println(concesionarias.indexOf(co) + "- " + co.toString());
+          }
+        }
+      }
+
+      case 2 -> {
+        if (clientes.isEmpty()) {
+          System.out.println("No hay ningun Cliente en lista");
+        } else {
+          System.out.println("Le mostraremos los Clientes en lista");
+          for (Cliente cl : clientes) {
+            System.out.println(clientes.indexOf(cl) + "- " + cl.toString());
+          }
+        }
+      }
+
+      case 3 -> {
+        if (vehiculos.isEmpty()) {
+          System.out.println("No hay ningun vehiculo disponible");
+        } else {
+          System.out.println("Le mostraremos los Clientes en lista");
+          for (Vehiculo ve : vehiculos) {
+            System.out.println(vehiculos.indexOf(ve) + "- " + ve.toString());
+          }
+        }
+      }
+    }
+  }
+
+  // Metodo para la compra/venta por parte de un cliente
+  public static void compraVentasPorCliente() {
+
+  }
 }
